11193faab7aa3a62b2a1a98458d774f4437d50ed
jessica teekasingh <jessi@jessicas-mbp-2.attlocal.net>
2020-04-24 22:06:21 -0400

    created game logic DOM

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,21 +22,37 @@ let cards =[
 ];
 let cardsInPlay =[];
 
-function checkForMatch(){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	  console.log("You found a match!");
-	} else {
-	  console.log("Sorry, try again.");
+function createBoard(){
+	for (let i = 0; i < cards.length; i++){
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', function(){
+			flipCard(cardElement)
+		})
+		document.getElementById('game-board').appendChild(cardElement)
 	}
 }
 
-function flipCard(index){
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  alert("You found a match!");
+	} else {
+	  alert("Sorry, try again.");
+	}
+}
+
+function flipCard(cardElement){
+	let index = cardElement.getAttribute('data-id')
 	console.log("User flipped " + cards[index].rank)
 	cardsInPlay.push(cards[index].rank);
 	checkForMatch()
 	console.log(cards[index].cardImage)
 	console.log(cards[index].suit)
+	cardElement.setAttribute('src', cards[index].cardImage);
 }
 
-flipCard(0)
-flipCard(2)
+// flipCard(0)
+// flipCard(2)
+
+createBoard()
