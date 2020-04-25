716ba6ac31fe1aa83ed7b4d380125bb1e7b4a3b8
jessica teekasingh <jessi@jessicas-mbp-2.attlocal.net>
2020-04-24 21:18:44 -0400

    created game logic objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-let cards =["queen","queen","king","king"];
+let cards =[
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+];
 let cardsInPlay =[];
 
 function checkForMatch(){
@@ -10,9 +31,11 @@ function checkForMatch(){
 }
 
 function flipCard(index){
-	console.log("User flipped " + cards[index])
-	cardsInPlay.push(cards[index]);
+	console.log("User flipped " + cards[index].rank)
+	cardsInPlay.push(cards[index].rank);
 	checkForMatch()
+	console.log(cards[index].cardImage)
+	console.log(cards[index].suit)
 }
 
 flipCard(0)
