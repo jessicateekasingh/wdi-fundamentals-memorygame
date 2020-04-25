9c00aeaa275b1b09c81070946639fc55d3ed4e6e
jessica teekasingh <jessi@jessicas-mbp-2.attlocal.net>
2020-04-24 20:57:41 -0400

    created game logic functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,19 @@
-console.log("Up and running!");
-
 let cards =["queen","queen","king","king"];
-
 let cardsInPlay =[];
-let cardOne = cards[0]; // first card 
-// let cardOne = "queen";
-cardsInPlay.push(cardOne);
 
-console.log("User flipped " + cardOne);
-
-let cardTwo = cards[1];
-// let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
+	  console.log("You found a match!");
+	} else {
+	  console.log("Sorry, try again.");
 	}
 }
+
+function flipCard(index){
+	console.log("User flipped " + cards[index])
+	cardsInPlay.push(cards[index]);
+	checkForMatch()
+}
+
+flipCard(0)
+flipCard(2)
